964960377504a86b224900f438ee9c55537fde06
Choi702 <jaeychoi1@gmail.com>
2020-10-01 21:23:23 -0700

    feature 2 hide element to do show

--- README.md
+++ README.md
@@ -11,3 +11,33 @@ Start time: 5:30pm
 Finish time: 7:30pm
 
 Actual time needed to complete: 2 Hours.
+
+
+
+
+Number and name of feature: feature number 2
+
+Estimate of time needed to complete: 1 hour.
+
+Start time: 7:38pm
+
+Finish time:
+
+Actual time needed to complete: 
+
+
+
+
+
+
+Number and name of feature: 
+
+Estimate of time needed to complete: 
+
+Start time: 
+
+Finish time: 
+
+Actual time needed to complete:
+
+
--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 let $template = $('#photo-template');
-let $container =$('#photo-container');
+let $container = $('#photo-container');
+let $dropdown = $('#dropdown');
 function Photo(img, title, desc, key, horns) {
   this.img = img;
   this.title = title;
@@ -12,26 +13,39 @@ function Photo(img, title, desc, key, horns) {
 
 }
 
+let keyWords = [];
+
+
 $.ajax('./data/page-1.json').then(data => {
-  
+
   data.forEach(photo => {
-    
+
     let photoObject = new Photo(photo.image_url, photo.title, photo.description, photo.keyword, photo.horns);
     let $newPhoto = $template.clone();
+    $newPhoto.attr('class', photoObject.key);
     $newPhoto.find('h2').text(photoObject.title);
     $newPhoto.find('p').text(photoObject.desc);
     $newPhoto.find('img').attr('src', photoObject.img);
-    $container.append($newPhoto);
-  
-    
-    
-    
-    
+    $container.append($newPhoto)
+    if (keyWords.indexOf(photoObject.key) == -1) {
+      keyWords.push(photoObject.key);
+      $dropdown.append(
+        $('<option></option>').text(photoObject.key)
+      )
+    }
 
-    
   });
 
 })
 
+$dropdown.change(function () {
 
+  $container.hide();
+
+  let val = $(this).val();
+  let $photoToShow = $('.' + val)
+  $photoToShow.show();
+ 
+  console.log($photoToShow);
+})
 
